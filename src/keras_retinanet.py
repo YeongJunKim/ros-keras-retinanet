31a0f23e84b858306efd6475a30b7785c8930c33
colson <dud3722000@naver.com>
2020-06-24 17:18:52 +0900

    .

--- src/keras_retinanet.py
+++ src/keras_retinanet.py
@@ -1,10 +1,11 @@
 #! /usr/bin/python3
 
+import cv2
+
 import rospy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 from cv_bridge.boost.cv_bridge_boost import getCvType
-import cv2
 
 import keras
 
@@ -36,69 +37,146 @@ setup_gpu(gpu)
 
 
 model_path = os.path.join('..', 'model', 'resnet50_coco_best_v2.1.0.h5')
-model = models.load_model('/home/colson/catkin_ws/src/ros_keras_retinanet/model/resnet50_coco_best_v2.1.0.h5', backbone_name='resnet50')
+#model = models.load_model('/home/colson/catkin_ws/src/ros-keras-retinanet/model/resnet50_coco_best_v2.1.0.h5', backbone_name='resnet50')
 labels_to_names = {0: 'person', 1: 'bicycle', 2: 'car', 3: 'motorcycle', 4: 'airplane', 5: 'bus', 6: 'train', 7: 'truck', 8: 'boat', 9: 'traffic light', 10: 'fire hydrant', 11: 'stop sign', 12: 'parking meter', 13: 'bench', 14: 'bird', 15: 'cat', 16: 'dog', 17: 'horse', 18: 'sheep', 19: 'cow', 20: 'elephant', 21: 'bear', 22: 'zebra', 23: 'giraffe', 24: 'backpack', 25: 'umbrella', 26: 'handbag', 27: 'tie', 28: 'suitcase', 29: 'frisbee', 30: 'skis', 31: 'snowboard', 32: 'sports ball', 33: 'kite', 34: 'baseball bat', 35: 'baseball glove', 36: 'skateboard', 37: 'surfboard', 38: 'tennis racket', 39: 'bottle', 40: 'wine glass', 41: 'cup', 42: 'fork', 43: 'knife', 44: 'spoon', 45: 'bowl', 46: 'banana', 47: 'apple', 48: 'sandwich', 49: 'orange', 50: 'broccoli', 51: 'carrot', 52: 'hot dog', 53: 'pizza', 54: 'donut', 55: 'cake', 56: 'chair', 57: 'couch', 58: 'potted plant', 59: 'bed', 60: 'dining table', 61: 'toilet', 62: 'tv', 63: 'laptop', 64: 'mouse', 65: 'remote', 66: 'keyboard', 67: 'cell phone', 68: 'microwave', 69: 'oven', 70: 'toaster', 71: 'sink', 72: 'refrigerator', 73: 'book', 74: 'clock', 75: 'vase', 76: 'scissors', 77: 'teddy bear', 78: 'hair drier', 79: 'toothbrush'}
 
+is_running = 0
+boxes = 0
+scores = 0
+labels = 0
 
-
+cap = cv2.VideoCapture(4)
 # Instantiate CvBridge
 bridge = CvBridge()
+image = 0
 
 def image_callback(msg):
-    print("Received an image!")
-    try:
-        # Convert your ROS Image message to OpenCV2
-        cv2_img = bridge.imgmsg_to_cv2(msg, "bgr8")
+    # print("Received an image!")
+    global is_running
+    global image
 
-        cv2_img2 = read_image_bgr('/home/colson/catkin_ws/src/ros_keras_retinanet/src/000000008021.jpg')
-        draw = cv2_img2.copy()
-        draw = cv2.cvtColor(draw, cv2.COLOR_BGR2RGB)
-
-        image = preprocess_image(cv2_img2)
-        image, scale = resize_image(image)
-
-        # process image
-        start = time.time()
-        boxes, scores, labels = model.predict_on_batch(np.expand_dims(image, axis=0))
-        print("processing time: ", time.time() - start)
-
-        # correct for image scale
-        boxes /= scale
-
-        # visualize detections
-        for box, score, label in zip(boxes[0], scores[0], labels[0]):
-            # scores are sorted so we can break
-            if score < 0.5:
-                break
-                
-            color = label_color(label)
-            
-            b = box.astype(int)
-            draw_box(draw, b, color=color)
-            
-            caption = "{} {:.3f}".format(labels_to_names[label], score)
-            draw_caption(draw, b, caption)
-            
-        plt.figure(figsize=(15, 15))
-        plt.axis('off')
-        plt.imshow(draw)
-        plt.show()
-
-    except CvBridgeError as e:
-        print(e)
+    if is_running == 1:
+        return;
     else:
-        # Save your OpenCV2 image as a jpeg 
-        cv2.imwrite('camera_image.jpeg', cv2_img)
+        is_running = 1
+
+    image = bridge.imgmsg_to_cv2(msg, "bgr8")
+
+    # try:
+    #     # Convert your ROS Image message to OpenCV2
+    #     image = bridge.imgmsg_to_cv2(msg, "bgr8")
+    #     #ret, image = cap.read()
+    #     #cv2.imwrite('out.jpg', image)
+    #     image = np.ascontiguousarray(image)
+        
+    #     draw = image.copy()
+    #     draw = cv2.cvtColor(draw, cv2.COLOR_BGR2RGB)
+
+    #     image = preprocess_image(image)
+    #     #image, scale = resize_image(image)
+    #     scale = 1
+        
+    #     # process image
+    #     start = time.time()
+    #     boxes, scores, labels = model.predict_on_batch(np.expand_dims(image, axis=0))
+    #     print("processing time: ", time.time() - start)
+
+    #     # correct for image scale
+    #     boxes /= scale
+
+    #     # visualize detections
+    #     for box, score, label in zip(boxes[0], scores[0], labels[0]):
+    #         # scores are sorted so we can break
+    #         if score < 0.5:
+    #             break
+                
+    #         color = label_color(label)
+            
+    #         b = box.astype(int)
+    #         draw_box(draw, b, color=color)
+            
+    #         caption = "{} {:.3f}".format(labels_to_names[label], score)
+    #         draw_caption(draw, b, caption)
+            
+    #     plt.figure(figsize=(15, 15))
+    #     plt.axis('off')
+    #     plt.imshow(draw)
+    #     plt.show()
+
+    # except CvBridgeError as e:
+    #     print(e)
+    # except ValueError as e:
+    #     print(e)
+    # except:
+    #     print("Unexpected error:", sys.exc_info()[0])
+    # else:
+    #     # Save your OpenCV2 image as a jpeg 
+    #     #cv2.imwrite('camera_image.jpeg', cv2_img)
+    #     print('good')
+    # is_running =0
+
 
 def main():
+    print('image_lisnter')
     rospy.init_node('image_listener')
     # Define your image topic
     image_topic = "/usb_cam/image_raw"
     # Set up your subscriber and define its callback
     rospy.Subscriber(image_topic, Image, image_callback, queue_size=1)
+    image_pub = rospy.Publisher("/output/image_raw", Image)
+    
+    model = models.load_model('/home/colson/catkin_ws/src/ros-keras-retinanet/model/resnet50_coco_best_v2.1.0.h5', backbone_name='resnet50')
+    labels_to_names = {0: 'person', 1: 'bicycle', 2: 'car', 3: 'motorcycle', 4: 'airplane', 5: 'bus', 6: 'train', 7: 'truck', 8: 'boat', 9: 'traffic light', 10: 'fire hydrant', 11: 'stop sign', 12: 'parking meter', 13: 'bench', 14: 'bird', 15: 'cat', 16: 'dog', 17: 'horse', 18: 'sheep', 19: 'cow', 20: 'elephant', 21: 'bear', 22: 'zebra', 23: 'giraffe', 24: 'backpack', 25: 'umbrella', 26: 'handbag', 27: 'tie', 28: 'suitcase', 29: 'frisbee', 30: 'skis', 31: 'snowboard', 32: 'sports ball', 33: 'kite', 34: 'baseball bat', 35: 'baseball glove', 36: 'skateboard', 37: 'surfboard', 38: 'tennis racket', 39: 'bottle', 40: 'wine glass', 41: 'cup', 42: 'fork', 43: 'knife', 44: 'spoon', 45: 'bowl', 46: 'banana', 47: 'apple', 48: 'sandwich', 49: 'orange', 50: 'broccoli', 51: 'carrot', 52: 'hot dog', 53: 'pizza', 54: 'donut', 55: 'cake', 56: 'chair', 57: 'couch', 58: 'potted plant', 59: 'bed', 60: 'dining table', 61: 'toilet', 62: 'tv', 63: 'laptop', 64: 'mouse', 65: 'remote', 66: 'keyboard', 67: 'cell phone', 68: 'microwave', 69: 'oven', 70: 'toaster', 71: 'sink', 72: 'refrigerator', 73: 'book', 74: 'clock', 75: 'vase', 76: 'scissors', 77: 'teddy bear', 78: 'hair drier', 79: 'toothbrush'}
+
     # Spin until ctrl + c
-    rospy.spin()
-        
+    #rospy.spin()
+    while(1):
+        global is_running
+        global image
+
+        if is_running == 1:        
+    
+            draw = image.copy()
+            draw = cv2.cvtColor(draw, cv2.COLOR_BGR2RGB)
+
+            image = preprocess_image(image)
+            #image, scale = resize_image(image)
+            scale = 1
+            
+            # process image
+            start = time.time()
+            boxes, scores, labels = model.predict_on_batch(np.expand_dims(image, axis=0))
+            print("processing time: ", time.time() - start)
+
+            # correct for image scale
+            boxes /= scale
+
+            # visualize detections
+            for box, score, label in zip(boxes[0], scores[0], labels[0]):
+                # scores are sorted so we can break
+                if score < 0.5:
+                    break
+                    
+                color = label_color(label)
+                
+                b = box.astype(int)
+                draw_box(draw, b, color=color)
+
+                caption = "{} {:.3f}".format(labels_to_names[label], score)
+                draw_caption(draw, b, caption)
+            
+            #print(draw)
+            
+            msg = CvBridge().cv2_to_imgmsg(draw)
+            image_pub.publish(msg)
+
+            # draw = cv2.cvtColor(draw, f
+            # plt.figure(figsize=(15, 15))
+            # plt.axis('off')
+            # plt.imshow(draw)
+            # plt.show()
+
+            is_running = 0
 
 if __name__ == '__main__':
     main()
